71373f2749b60d41970572c4b4c1c42b45b5f112
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-06-08 14:10:22 +0300

    initialize prisma with data models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
