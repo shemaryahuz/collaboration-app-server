7c54dfa492049c2c955764aa19ae4a59cc1cb5eb
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-06-15 21:48:01 +0300

    add users module and service. update user model to have password

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { UsersService } from './users/users.service.js';
+import { UsersModule } from './users/users.module.js';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PrismaModule,
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule { }
---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service.js';
+
+@Injectable()
+export class UsersService {
+    constructor(private prisma: PrismaService) { }
+
+    async findOneByEmail(email: string): Promise<{ id: string; email: string; passwordHash: string } | null> {
+        return this.prisma.user.findUnique({
+            where: { email },
+        });
+    }
+
+    async createUser(email: string, passwordHash: string, name?: string): Promise<{ id: string; email: string; passwordHash: string }> {
+        return this.prisma.user.create({
+            data: { email, passwordHash, name },
+        });
+    }
+}
