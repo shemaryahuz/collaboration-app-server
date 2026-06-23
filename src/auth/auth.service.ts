d383e3b9af6ac2d4e67644b945f001d326ed7952
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-06-23 23:45:53 +0300

    fix error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 
@@ -21,11 +21,11 @@ export class AuthService {
         return this.jwtService.sign(payload);
     }
 
-    async signUp(signupDto: SignupDto): Promise<string> {
+    async signup(signupDto: SignupDto): Promise<string> {
 
         const existingUser = await this.usersService.findOneByEmail(signupDto.email);
         if (existingUser) {
-            throw new Error('Email already exists');
+            throw new ConflictException('Email already exists');
         }
 
         const user = await this.usersService.createUser(
@@ -40,12 +40,12 @@ export class AuthService {
 
         const user = await this.usersService.findOneByEmail(email);
         if (!user) {
-            throw new Error('Invalid email or password');
+            throw new UnauthorizedException('Invalid email or password');
         }
 
         const isMatch = await this.comparePasswords(password, user.passwordHash);
         if (!isMatch) {
-            throw new Error('Invalid email or password');
+            throw new UnauthorizedException('Invalid email or password');
         }
 
         return await this.generateToken(user);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Server is running on http://localhost:${port}`);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
+  await app.listen(PORT);
+
+  console.log(`Server is running on http://localhost:${PORT}`);
 }
 bootstrap();
