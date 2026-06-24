2c43d7fc42800912784ac7439dd0a2c78aeb1059
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-06-24 12:37:28 +0300

    update app and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { UsersService } from './users/users.service.js';
 import { UsersModule } from './users/users.module.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true, }),
     PrismaModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService, UsersService],
+  providers: [AppService],
 })
 export class AppModule { }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
-export class AuthModule {}
+import { UsersModule } from 'src/users/users.module.js';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => ({
+                secret: configService.get<string>('JWT_SECRET', 'fallback_secret_key'),
+                signOptions: {
+                    expiresIn: configService.get<string>('JWT_EXPIRES_IN', '1d') as any,
+                }
+            })
+        })
+    ],
+    controllers: [AuthController],
+    providers: [AuthService],
+})
+export class AuthModule { }
