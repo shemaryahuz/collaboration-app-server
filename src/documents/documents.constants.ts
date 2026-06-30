0f8ac568555b3d39f95424f2db555f66f04d6c15
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-06-30 15:09:51 +0300

    add success message for document

--- src/documents/documents.constants.ts
+++ src/documents/documents.constants.ts
@@ -1,7 +1,11 @@
-import { title } from "process";
-
 export const DOCUMENT_ERROR_MESSAGES = {
     titleRequired: 'Title is required',
     documentNotFound: 'Document not found',
     documentsNotFound: 'Documents not found',
 }
+
+export const DOCUMENT_SUCCESS_MESSAGES = {
+    documentCreated: 'Document created successfully',
+    documentRetrieved: 'Document retrieved successfully',
+    documentsRetrieved: 'Documents retrieved successfully',
+} as const;
